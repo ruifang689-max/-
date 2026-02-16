b2c2962d11f35ab7485d727674e4797a596c32e1
ruifang689-max <ruifang689@gmail.com>
2026-02-16 14:31:14 +0800

    Refactor caching logic in service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
 const CACHE_NAME = 'ruifang-app-v1';
-const urlsToCache = [ './index.html', './manifest.json' ];
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
 
+// 安裝時快取檔案
 self.addEventListener('install', event => {
-  event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)) );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// 攔截請求，若斷網則提供快取
 self.addEventListener('fetch', event => {
-  event.respondWith( caches.match(event.request).then(response => response || fetch(event.request)) );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
