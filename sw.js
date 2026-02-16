8aed108e01456bcda688957c1e9a7532e20bfb7c
ruifang689-max <ruifang689@gmail.com>
2026-02-16 15:03:05 +0800

    Update service worker cache version and simplify code

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE_NAME = 'ruifang-app-v1';
+const CACHE_NAME = 'ruifang-app-v2';
 const urlsToCache = [
+  './',
   './index.html',
   './manifest.json'
 ];
 
-// 安裝時快取檔案
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 攔截請求，若斷網則提供快取
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
