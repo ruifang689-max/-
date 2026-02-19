7aa89259bc8fc71e93491e8563b693ce84718c27
ruifang689-max <ruifang689@gmail.com>
2026-02-19 22:23:41 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 import { initFirebase } from './modules/firebase-sync.js';// 我們剛剛新增的雲端模組
 import { state } from './core/store.js';
 import { initMap, toggleLayer } from './core/map.js';
-import { initMarkers } from './modules/markers.js'; // 👈 就是少了這一行！
+import { loadMarkers } from './modules/markers.js'; // 👈 就是少了這一行！
 import { fetchWeather } from './modules/weather.js';
 import { initGPS } from './modules/gps.js';
 import { initAnnouncer } from './modules/announcer.js';
@@ -67,7 +67,7 @@ if (document.readyState === 'complete' || document.readyState === 'interactive')
 
 document.addEventListener('DOMContentLoaded', () => {
     initMap();
-    initMarkers();
+    loadMarkers(); // 呼叫正確的名稱
     initUI();
     
     // 🌟 啟動 Firebase 雲端模組
