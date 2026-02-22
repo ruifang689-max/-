4dc348d98c8e922289de509c205e164516267d31
ruifang689-max <ruifang689@gmail.com>
2026-02-22 17:12:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js (v642) - 企業級動態快取引擎
+// sw.js (v643) - 企業級動態快取引擎
 
-const CACHE_VERSION = 'v642';
+const CACHE_VERSION = 'v643';
 const STATIC_CACHE = `rf-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `rf-dynamic-${CACHE_VERSION}`;
 const MAP_CACHE = `rf-map-${CACHE_VERSION}`;
